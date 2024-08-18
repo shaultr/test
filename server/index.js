cbbb344cd12128f2d8e4c59967d63eb4ca43b948
unknown <a0548428751@gmail.com>
2024-08-18 17:54:34 +0300

    versel

--- server/index.js
+++ server/index.js
@@ -1,25 +1,11 @@
-// const express =  require ('express'),
-// cors = require ('cors');
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-
-// app.use('/users', require('./users'));
-
-
-// app.listen(8000, ()=>{console.log('server is running on port 8000');
-// })
-
-// index.js
-const express = require('express');
-const cors = require('cors');
+const express =  require ('express'),
+cors = require ('cors');
 const app = express();
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-app.get('/users', (req, res) => {
-  res.json({ message: 'Hello from users endpoint!' });
-});
+app.use('/users', require('./users'));
 
-module.exports = app;
+
+app.listen(8000, ()=>{console.log('server is running on port 8000');
+})
