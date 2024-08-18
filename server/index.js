219344b24683c4d9d5dce4f993334995d0d4acf4
unknown <a0548428751@gmail.com>
2024-08-18 20:24:06 +0300

    hooo

--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ app.use(express.json());
 app.use(cors());
 
 // app.use('/users', require('./api/users'));
-app.use('/users', ()=>{
+app.use('/users', (req, res)=>{
     console.log("Welcome");
+    res.send("hooo",200);
     
 });
 
 
-
+// app.listen(8000, ()=>{console.log('server is running on port 8000');
+// })
 
 
 module.exports = app;
