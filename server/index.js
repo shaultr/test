46d661062dc741baef7adc45651d9bb8d544f93f
unknown <a0548428751@gmail.com>
2024-08-18 17:46:58 +0300

    app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express =  require ('express'),
 cors = require ('cors');
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // app.use('/users', require('./users'));
 
