2743db4ae963a30633aba0f65558aa9475197127
unknown <a0548428751@gmail.com>
2024-08-18 17:49:13 +0300

    app22

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express =  require ('express'),
-cors = require ('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const express =  require ('express'),
+// cors = require ('cors');
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
 // app.use('/users', require('./users'));
 
@@ -10,8 +10,16 @@ app.use(express.json());
 // app.listen(8000, ()=>{console.log('server is running on port 8000');
 // })
 
+// index.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/users', (req, res) => {
-    res.json({ message: 'Hello from users endpoint!' });
-  });
-  
-  module.exports = app;
+  res.json({ message: 'Hello from users endpoint!' });
+});
+
+module.exports = app;
