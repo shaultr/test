67f3df1fd42a9a7f0da638f41abe4099a4994d65
unknown <a0548428751@gmail.com>
2024-08-19 18:58:50 +0300

    home

--- server/index.js
+++ server/index.js
@@ -5,15 +5,20 @@ app.use(express.json());
 app.use(cors());
 
 // app.use('/users', require('./api/users'));
-app.use('/users', (req, res)=>{
-    console.log("Welcome");
-    res.send("hooo",200);
-    
+
+app.get('/', (req, res) => {
+  res.send('ברוך הבא לדף הבית!');
 });
 
+app.use('/users',   
+    (req, res) => {
+     const response = { message: 'Hello from users' };
+     res.json(response); 
+   });
 
-// app.listen(8000, ()=>{console.log('server is running on port 8000');
-// })
+
+app.listen(8000, ()=>{console.log('server is running on port 8000');
+})
 
 
-module.exports = app;
+// module.exports = app;
